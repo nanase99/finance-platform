5ace8b370ebf277fa2620498d9eb6e3089e36f11
nanase <sgmdq601@gmail.com>
2024-06-04 13:13:28 +0900

    add: create Header

---
+++ src/components/ui/navButton.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Button } from "./button";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  href: string;
+  label: string;
+  isActive?: boolean;
+};
+
+export const NavButton = ({ href, label, isActive }: Props) => {
+  return (
+    <Button
+      size="sm"
+      variant="outline"
+      asChild
+      className={cn(
+        "font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent focus:bg-white/30 outline-none text-white transition",
+        isActive ? "bg-white/10" : "bg-transparent",
+      )}
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
