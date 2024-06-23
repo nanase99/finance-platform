3ea33f07672c912560b5d0c0c77ffacd98eed92c
nanase <sgmdq601@gmail.com>
2024-06-23 15:21:16 +0900

    fix: drizzle-kit version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env.local" });
 
 export default defineConfig({
-  schema: "./db/schema.ts",
-  driver: "pg",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!
   },
   verbose: true,
-  strict: true,
+  strict: true
 });
