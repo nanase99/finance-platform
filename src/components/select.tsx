30c8b94a3055115d47a56a5492f31203003f9523
nanase <sgmdq601@gmail.com>
2024-06-28 09:06:12 +0900

    add: modify the select component

---
+++ src/components/select.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useMemo } from "react";
+import type { SingleValue } from "react-select";
+import CreatableSelect from "react-select/creatable";
+
+type Props = {
+  onChange: (value?: string) => void;
+  onCreate?: (value: string) => void;
+  options?: { label: string; value: string }[];
+  value?: string | null | undefined;
+  disabled?: boolean;
+  placeholder?: string;
+};
+
+export const Select = ({
+  value,
+  onChange,
+  disabled,
+  onCreate,
+  options,
+  placeholder
+}: Props) => {
+  const onSelect = (option: SingleValue<{ label: string; value: string }>) =>
+    onChange(option?.value);
+
+  const formattedValue = useMemo(() => {
+    return options?.find((option) => option.value === value);
+  }, [options, value]);
+
+  return (
+    <CreatableSelect
+      value={formattedValue}
+      onChange={onSelect}
+      options={options}
+      onCreateOption={onCreate}
+      placeholder={placeholder}
+      isDisabled={disabled}
+      className="text-sm h-10"
+      styles={{
+        control: (base) => ({
+          ...base,
+          borderColor: "#2e8f0",
+          ":hover": {
+            borderColor: "#e2e8f0"
+          }
+        })
+      }}
+    />
+  );
+};
