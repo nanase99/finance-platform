bb0defa9bf4f87a9f87d8ba978f5282dd4aa5654
nanase <sgmdq601@gmail.com>
2024-07-06 23:33:23 +0900

    add: data card

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+import { DataGrid } from "@/components/dataGrid";
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/useNewAccount";
-
-export default function Home() {
-  const { onOpen } = useNewAccount();
+export default function DashboardPage() {
   return (
-    <div>
-      Home page<Button onClick={onOpen}>Add an Account </Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
